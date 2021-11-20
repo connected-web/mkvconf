8727bdb8e09d2301045834bfa36776dc968271ab
John Beech <johnbeech.github@mkv25.net>
2021-11-20 23:59:46 +0000

    Make a more complex test example

--- test/parser.test.js
+++ test/parser.test.js
@@ -2,6 +2,8 @@ const { expect } = require('chai')
 const loadFixture = require('./helpers/loadFixture')
 const mkvconf = require('../')
 
+const fs = require('fs')
+
 describe('Parser', () => {
   it('should correctly parse the fruit fixture', () => {
     const input = loadFixture('fruit.file')
@@ -14,6 +16,14 @@ describe('Parser', () => {
     const input = loadFixture('item.file')
     const expected = loadFixture('item.json')
     const actual = mkvconf.parse(input)
+    expect(actual).to.deep.equal(expected)
+  })
+
+  it('should correctly parse the mixed fixture', () => {
+    const input = loadFixture('mixed.file')
+    const expected = loadFixture('mixed.json')
+    const actual = mkvconf.parse(input)
+    fs.writeFileSync('./example.json', JSON.stringify(actual, null, 2), 'utf8')
     expect(actual).to.deep.equal(expected)
   })
 })
